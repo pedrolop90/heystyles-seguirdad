dff729cebd908205cea96477a6ef920d89d1dc71
pedrolop90 <pedrolop90@hotmail.com>
2019-10-05 11:25:39 -0500

    clients.....

---
+++ heystyles-seguridad-api/src/main/java/com/heystyles/seguridad/api/config/ClientProperties.java
@@ -0,0 +1,31 @@
+package com.heystyles.seguridad.api.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component
+@Validated
+@ConfigurationProperties("client")
+public class ClientProperties {
+
+    private int connectionTimeout;
+
+    private int readTimeout;
+
+    public int getConnectionTimeout() {
+        return connectionTimeout;
+    }
+
+    public void setConnectionTimeout(int connectionTimeout) {
+        this.connectionTimeout = connectionTimeout;
+    }
+
+    public int getReadTimeout() {
+        return readTimeout;
+    }
+
+    public void setReadTimeout(int readTimeout) {
+        this.readTimeout = readTimeout;
+    }
+}
